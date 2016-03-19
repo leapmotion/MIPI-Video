fd2edb1e93fa26500d4d2715b7f558be855ad943
Jason Lokerson <cmercenary@gmail.com>
2016-03-18 18:14:18 -0700

    Add multi-load support to LeapLibrary

--- src/LeapLibrary/LeapLibrary.h
+++ src/LeapLibrary/LeapLibrary.h
@@ -38,4 +38,30 @@ public:
   /// The address, or nullptr if it was not found
   /// </returns>
   virtual entrypoint Find(const char* name) const = 0;
+
+  struct entrypoint_name {
+    template<typename T>
+    entrypoint_name(const char* name, T*& pfn) :
+      name(name),
+      ppfn(reinterpret_cast<void**>(&pfn))
+    {}
+
+    const char* name;
+    void** ppfn;
+  };
+
+  /// <summary>
+  /// Utility method, loads all of the specified name and function pairs
+  /// </summary>
+  /// <remarks>
+  /// Each of the passed load requests will be non-null on successful return.  If some entrypoints
+  /// are missing, this function will throw an exception.
+  /// </remarks>
+  void LoadAll(std::initializer_list<entrypoint_name> names) const {
+    for(const entrypoint_name& name : names) {
+      *name.ppfn = Find(name.name);
+      if(!*name.ppfn)
+        throw std::runtime_error(std::string{ "Failed to find entrypoint " } + name.name);
+    }
+  }
 };
